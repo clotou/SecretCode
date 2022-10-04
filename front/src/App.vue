ebdd3271ee91e068fdeb37ab184612c35e0d509f
clotilde lacaille <lacailleclotilde@gmail.com>
2022-10-04 19:06:33 +0200

    first page ok

---
+++ front/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+  <router-view />
+  </div>
+  <!-- <CodeInput/> -->
+</template>
+
+<script>
+import CodeInput from './components/CodeInput.vue';
+import WinPage from './components/WinPage.vue';
+
+// export default {
+//   name: "App",
+//   data() {
+//     return {
+//     };
+//   },
+//   components: { CodeInput, winPage }
+// }
+
+export default {
+  name: 'App',
+  components: {
+    CodeInput,
+    WinPage
+  }
+}
+</script>
---
+++ front/src/components/WinPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Félicitations!!!</h1>
+  <p>Vous êtes parvenu à retrouver le code perdu. Vous avez fait preuve d’un bon esprit d’équipe. la mission touche à sa
+    fin mais il vous reste une dernière chose à accomplir.</p><br>
+  <p>Veuillez retourner à la base, dans le premier espace dont le lien est ci-dessous.
+    Vous devez changer la couleur des vêtements de votre avatar en bleu.
+    Ensuite dirigez vous au à l’étage inférieur.
+    Alignez vous en ordre de dégradé de bleu contre le mur jonché du logo promo.dev.
+    A tout de suite dans la base
+  </p>
+</template>
